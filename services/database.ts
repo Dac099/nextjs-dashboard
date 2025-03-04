bdd0383c767a4630de8f8eaa1d043f61e46d9441
d.ceja <d.ceja@ynemx.com>
2025-03-04 23:36:28 +0000

    changes to prod

--- services/database.ts
+++ services/database.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 config();
 const isDevelopment: boolean = process.env.ENVIRONMENT === 'DEVELOPMENT';
 const sqlStringConnection: string = (isDevelopment ? process.env.CONNECTION_STRING_DEV : process.env.CONNECTION_STRING_PROD) as string;
-
-const connection: sql.ConnectionPool = new sql.ConnectionPool(sqlStringConnection);
+console.log(sqlStringConnection);
+const connection: sql.ConnectionPool = new sql.ConnectionPool(process.env.CONNECTION_STRING_PROD);
 
 export default connection; 
