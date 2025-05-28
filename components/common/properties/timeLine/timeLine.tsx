70256d72001de8148e5850fd5dff946c872ba2bd
David Ceja Zapata <d.ceja@ynemx.com>
2025-05-28 16:21:06 -0600

    REFAC (timeLine component): Simplify styles and improve date handling logic; FIX (Percentage Component): Add margin to container to prevent content overflow

--- components/common/properties/timeLine/timeLine.tsx
+++ components/common/properties/timeLine/timeLine.tsx
@@ -1,112 +1,114 @@
-'use client';
+"use client";
 
-import styles from './styles.module.css';
-import Calendar from 'react-calendar';
+import styles from "./styles.module.css";
+import { Calendar } from "primereact/calendar";
 import { useEffect, useState, useRef } from "react";
-import { TableValue } from '@/utils/types/groups';
-import { useParams } from 'next/navigation';
-import { setTableValue } from '@/actions/groups';
-import { Actions } from '@/utils/types/roles';
-import { roleAccess } from '@/utils/userAccess';
-import { useRoleUserActions } from '@/stores/roleUserActions';
+import { TableValue } from "@/utils/types/groups";
+import { useParams } from "next/navigation";
+import { setTableValue } from "@/actions/groups";
+import { ProgressBar } from "primereact/progressbar";
+import { useRoleUserActions } from "@/stores/roleUserActions";
+import { Nullable } from "primereact/ts-helpers";
+import { calculatePercentageBetweenDates } from "@/utils/helpers";
+import { formatTimeLineItemValue } from "@/utils/helpers";
 
 type Props = {
-    value: TableValue;
-    columnId: string;
-    itemId: string;
+  value: TableValue;
+  columnId: string;
+  itemId: string;
 };
 
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
-
+/**
+ * Componente `TimeLine`
+ *
+ * Renderiza un calendario de selección de rango de fechas y una barra de progreso que indica el porcentaje de avance entre dos fechas seleccionadas.
+ * Permite seleccionar un rango de fechas y muestra el rango seleccionado en formato legible. El progreso se calcula en base a las fechas seleccionadas.
+ *
+ * @param props - Propiedades del componente.
+ * @param props.value - Objeto de tipo TableValue que contiene un campo `value`, el cual es un JSON que representa un arreglo de dos fechas (`[fechaInicial, fechaFinal]`), donde la primera posición es la fecha inicial y la segunda la fecha final.
+ * @param props.columnId - Identificador de la columna asociada.
+ * @param props.itemId - Identificador del ítem asociado.
+ *
+ * @returns Un componente visual que permite seleccionar un rango de fechas y muestra el progreso entre ellas.
+ */
 export const TimeLine = ({ value, columnId, itemId }: Props) => {
-    const userActions = useRoleUserActions((state) => state.userActions);
-    const {id: boardId, viewId} = useParams();
-    const [ showCalendar, setShowCalendar ] = useState<boolean>(false);
+  const calendarRef = useRef<Calendar>(null);
+  const userActions = useRoleUserActions((state) => state.userActions);
+  const { id: boardId, viewId } = useParams() as { id: string; viewId: string };
+  const [percentage, setPercentage] = useState<number>(0);
+  const [datesLabel, setDatesLabel] = useState<string>("Sin definir");
+  const [dates, setDates] = useState<Nullable<Date | (Date | null)[]>>(
+    formatTimeLineItemValue(value)
+  );
 
-    const defaultValue: [ValuePiece, ValuePiece] = value.value
-        ? JSON.parse(value.value).map(date => new Date(date))
-        : [new Date(), new Date(new Date().setMonth(new Date().getMonth() + 2))];
-
-    const [ dates, setDates ] = useState<Value>(defaultValue);
-    const prevDatesRef = useRef<Value>(defaultValue);
-    const isInitialMount = useRef(true);
-    
-    // Memoizar estos cálculos para evitar recálculos innecesarios
-    const currentDate = new Date();
-    const startDateTime = dates[0] ? new Date(dates[0].toString()).getTime() : 0;
-    const endDateTime = dates[1] ? new Date(dates[1].toString()).getTime() : 0;
-    const totalTime = endDateTime - startDateTime;
-    const completedTime = currentDate.getTime() - startDateTime;
-    const percentage = Math.max(0, Math.min(100, Math.round(100 * (completedTime / totalTime))));
-
-    // Este efecto maneja la actualización solo cuando las fechas cambian realmente
-    useEffect(() => {
-        // Ignorar el primer render
-        if (isInitialMount.current) {
-            isInitialMount.current = false;
-            return;
-        }
-
-        // Solo actualizar si las fechas han cambiado y el calendario no está visible
-        if (!showCalendar && JSON.stringify(dates) !== JSON.stringify(prevDatesRef.current)) {
-            // Actualizar la referencia para evitar actualizaciones innecesarias
-            prevDatesRef.current = dates;
-            
-            setTableValue(
-                boardId as string,
-                viewId as string,
-                itemId,
-                columnId,
-                JSON.stringify(dates)
-            );
-        }
-    }, [dates, showCalendar, boardId, viewId, itemId, columnId, value.id]);
-
-    const handleCalendarToggle = () => {
-        if(userActions.includes('update')){
-            setShowCalendar(!showCalendar);
-        }
-    };
-
-    const handleDatesChange = (newDates: Value) => {
-        setDates(newDates);
-        // Al seleccionar fechas, cerramos el calendario
-        setShowCalendar(false);
-    };
-
-    function formatDate(date: Value): string {
-        if (!date) return '';
-        
-        const months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        const jsDate = new Date(date.toString());
-        const month = months[jsDate.getMonth()];
-        const year = jsDate.getFullYear();
-
-        return `${month}/${year}`;
+  useEffect(() => {
+    if (
+      (Array.isArray(dates) && dates.some((date) => date === null)) ||
+      !dates
+    ) {
+      return;
     }
 
-    return (
-        <article className={styles.container}>
-            { showCalendar &&
-                <section className={styles['calendar-container']}>
-                    <Calendar selectRange={true} onChange={handleDatesChange} value={dates}/>
-                </section>
-            }
-
-            <section className={styles['time-line']} onClick={handleCalendarToggle}>
-                <p>{dates[0] && dates[1] ? `${formatDate(dates[0])} - ${formatDate(dates[1])}` : ''}</p>
-                <div className={styles['progress-bg']}></div>
-                <div
-                    className={styles['progress-bar']}
-                    style={{ 
-                        width: `${percentage}%`, 
-                        borderRadius: percentage < 94 ? '15px 0 0 15px' : '15px', 
-                        maxWidth: '100%',
-                        display: `${percentage <= 0 ? 'none' : 'block'}`
-                    }}
-                ></div>
-            </section>
-        </article>
+    setPercentage(calculatePercentageBetweenDates(dates as Date[]));
+    const formatedDates = (dates as Date[]).map((date) =>
+      date.toLocaleDateString("en-EN", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })
     );
-}
+    setDatesLabel(formatedDates.join(" - "));
+  }, [dates]);
+
+  const handleSelectDates = async (dates: Date[]) => {
+    if (!dates || (Array.isArray(dates) && dates.some((date) => date === null)))
+      return;
+
+    await setTableValue(
+      boardId,
+      viewId,
+      itemId,
+      columnId,
+      dates ? JSON.stringify(dates.map((date) => date.toISOString())) : ""
+    );
+  };
+
+  const handleShowCalendar = () => {
+    if (!userActions?.includes("update") || !userActions?.includes("create"))
+      return;
+    calendarRef.current?.show();
+  };
+
+  return (
+    <article className={styles.container}>
+      <section className={styles.calendarContainer}>
+        <Calendar
+          value={dates}
+          onChange={(e) => {
+            setDates(e.value);
+            handleSelectDates(e.value as Date[]);
+          }}
+          selectionMode="range"
+          readOnlyInput
+          hideOnRangeSelection
+          ref={calendarRef}
+          inputStyle={{ width: "100%", height: "100%" }}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </section>
+      <section className={styles.progressbarContainer}>
+        <>
+          <ProgressBar
+            value={percentage}
+            style={{ width: "100%", height: "100%", cursor: "pointer" }}
+            showValue={false}
+            onClick={handleShowCalendar}
+          ></ProgressBar>
+          <p className={styles.datesLabel} onClick={handleShowCalendar}>
+            {datesLabel}
+          </p>
+        </>
+      </section>
+    </article>
+  );
+};
--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -1,17 +1,21 @@
 import { Item, ProjectFormData, ValueDB } from "@/utils/types/projectDetail";
-import { SubItem, TableValue } from './types/groups';
-import { Task } from './types/items';
-import { v4 as uuidV4 } from 'uuid';
+import { SubItem, TableValue } from "./types/groups";
+import { Task } from "./types/items";
+import { v4 as uuidV4 } from "uuid";
 
 export function formatDate(date: Date): string {
-  const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'short', year: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
 }
 
 export function groupItemsByType(data: ValueDB[]): Item[] {
   const groupedByItemId: Record<string, Partial<Item>> = {};
 
-  data.forEach(item => {
+  data.forEach((item) => {
     const { itemId, columnName, value, itemName } = item;
 
     if (!groupedByItemId[itemId]) {
@@ -22,66 +26,73 @@ export function groupItemsByType(data: ValueDB[]): Item[] {
     }
 
     switch (columnName) {
-      case 'Total':
+      case "Total":
         groupedByItemId[itemId].total = parseFloat(JSON.parse(value));
         break;
-      case 'Invoice':
+      case "Invoice":
         groupedByItemId[itemId].invoice = parseFloat(JSON.parse(value));
         break;
-      case 'Payment date':
+      case "Payment date":
         const paymentDateClean = JSON.parse(value);
         groupedByItemId[itemId].paymentDate = new Date(paymentDateClean);
         break;
-      case 'Paid':
+      case "Paid":
         groupedByItemId[itemId].paid = parseFloat(JSON.parse(value));
         break;
-      case 'Invoice number':
+      case "Invoice number":
         groupedByItemId[itemId].invoiceNumber = JSON.parse(value);
         break;
-      case 'Status':
+      case "Status":
         try {
           groupedByItemId[itemId].status = JSON.parse(value);
         } catch (e) {
           console.error(`Error al parsear status para el item ${itemId}:`, e);
-          groupedByItemId[itemId].status = { color: '', text: '' };
+          groupedByItemId[itemId].status = { color: "", text: "" };
         }
         break;
-      case 'Last Updated':
+      case "Last Updated":
         const lastUpdateClean = JSON.parse(value);
         groupedByItemId[itemId].lastUpdate = new Date(lastUpdateClean);
         break;
       default:
-        console.warn(`Columna no reconocida: ${columnName} para el item ${itemId}`);
+        console.warn(
+          `Columna no reconocida: ${columnName} para el item ${itemId}`
+        );
         break;
     }
   });
 
-  return Object.values(groupedByItemId).map(item => {
+  return Object.values(groupedByItemId).map((item) => {
     return {
       id: item.id,
-      name: item.name || 'Sin nombre',
+      name: item.name || "Sin nombre",
       total: item.total || 0,
       invoice: item.invoice || 0,
       paymentDate: item.paymentDate || new Date(),
       paid: item.paid || 0,
-      invoiceNumber: item.invoiceNumber || '',
-      status: item.status || { color: '', text: '' },
+      invoiceNumber: item.invoiceNumber || "",
+      status: item.status || { color: "", text: "" },
       lastUpdate: item.lastUpdate || new Date(),
     } as Item;
   });
 }
-export function subItemValueByColumnId(columnId: string, subItem: SubItem): TableValue {
-  const tableValue = subItem
-    .values
-    .find((value) => value && value.columnId === columnId) as TableValue;
+export function subItemValueByColumnId(
+  columnId: string,
+  subItem: SubItem
+): TableValue {
+  const tableValue = subItem.values.find(
+    (value) => value && value.columnId === columnId
+  ) as TableValue;
 
-  return tableValue ?? {
-    id: '',
-    itemId: subItem.id,
-    columnId: columnId,
-    value: '',
-    groupId: ''
-  };
+  return (
+    tableValue ?? {
+      id: "",
+      itemId: subItem.id,
+      columnId: columnId,
+      value: "",
+      groupId: "",
+    }
+  );
 }
 
 export function findParentKeyBySubItemId(
@@ -89,7 +100,7 @@ export function findParentKeyBySubItemId(
   subItemId: string
 ): string | null {
   for (const [key, subItems] of subItemsMap) {
-    if (subItems.some(subItem => subItem.id === subItemId)) {
+    if (subItems.some((subItem) => subItem.id === subItemId)) {
       return key;
     }
   }
@@ -102,7 +113,7 @@ export function findParentKeyByValueId(
 ): string | null {
   for (const [key, subItems] of subItemsMap) {
     for (const subItem of subItems) {
-      if (subItem.values.some(value => value.id === valueId)) {
+      if (subItem.values.some((value) => value.id === valueId)) {
         return key;
       }
     }
@@ -111,20 +122,20 @@ export function findParentKeyByValueId(
 }
 
 export function extractTasksFromHTML(htmlString: string): Task[] {
-  const tempDiv = document.createElement('div');
+  const tempDiv = document.createElement("div");
   tempDiv.innerHTML = htmlString;
 
   const taskItems = tempDiv.querySelectorAll('li[data-type="taskItem"]');
 
   const tasks = Array.from(taskItems).map((item) => {
-    const completed = item.getAttribute('data-checked') === 'true';
-    const message = item.querySelector('div p')?.textContent || '';
+    const completed = item.getAttribute("data-checked") === "true";
+    const message = item.querySelector("div p")?.textContent || "";
     const id = uuidV4();
 
     return {
       id,
       message,
-      completed
+      completed,
     };
   });
 
@@ -133,106 +144,132 @@ export function extractTasksFromHTML(htmlString: string): Task[] {
 
 export function getDateSQLFormat() {
   const now = new Date();
-  
+
   // Formato YYYY-MM-DD HH:MM:SS.mmm
   const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-  const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
-  
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const milliseconds = String(now.getMilliseconds()).padStart(3, "0");
+
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
-export function validateProjectFormData(projectData: ProjectFormData): string[] {
+export function validateProjectFormData(
+  projectData: ProjectFormData
+): string[] {
   const errors: string[] = [];
-  
-  if(!projectData.projectFolio){
-    errors.push('Se requiere folio de proyecto');
+
+  if (!projectData.projectFolio) {
+    errors.push("Se requiere folio de proyecto");
   }
 
-  if(!projectData.poClient){
-    errors.push('Se requiere PO de cliente');
+  if (!projectData.poClient) {
+    errors.push("Se requiere PO de cliente");
   }
 
-  if(!projectData.projectName){
-    errors.push('Se requiere nombre de proyecto');
+  if (!projectData.projectName) {
+    errors.push("Se requiere nombre de proyecto");
   }
 
-  if(!projectData.quoteItem?.quote){
-    errors.push('Se requiere número de cotización');
+  if (!projectData.quoteItem?.quote) {
+    errors.push("Se requiere número de cotización");
   }
 
-  if(projectData.totalBudget < 0){
-    errors.push('Presupuesto total debe ser mayor a 0');
+  if (projectData.totalBudget < 0) {
+    errors.push("Presupuesto total debe ser mayor a 0");
   }
 
-  if(!projectData.currency){
-    errors.push('Se requiere moneda');
+  if (!projectData.currency) {
+    errors.push("Se requiere moneda");
   }
 
-  if(!projectData.sector){
-    errors.push('Se requiere sector');
+  if (!projectData.sector) {
+    errors.push("Se requiere sector");
   }
 
-  if(!projectData.status){
-    errors.push('Se requiere estatus');
+  if (!projectData.status) {
+    errors.push("Se requiere estatus");
   }
 
-  if(!projectData.projectManagerId || !projectData.assemblerId || !projectData.electricalDesignerId || !projectData.mechanicalDesignerId) {
-    errors.push('No se definieron todos los responsables');
+  if (
+    !projectData.projectManagerId ||
+    !projectData.assemblerId ||
+    !projectData.electricalDesignerId ||
+    !projectData.mechanicalDesignerId
+  ) {
+    errors.push("No se definieron todos los responsables");
   }
 
-  if(!projectData.poDate){
-    errors.push('Se requiere fecha de PO');
+  if (!projectData.poDate) {
+    errors.push("Se requiere fecha de PO");
   }
 
-  if(!projectData.deadline){
-    errors.push('Se requiere fecha fin');
+  if (!projectData.deadline) {
+    errors.push("Se requiere fecha fin");
   }
 
-  if(!projectData.kickOffDate){
-    errors.push('Se requiere fecha de kickoff');
+  if (!projectData.kickOffDate) {
+    errors.push("Se requiere fecha de kickoff");
   }
 
-  if(!projectData.proposedWeeks){
-    errors.push('Se requiere semanas propuestas');
+  if (!projectData.proposedWeeks) {
+    errors.push("Se requiere semanas propuestas");
   }
 
   return errors;
 }
 
-export function validateProjectBudgetAndTime(projectData: ProjectFormData): ProjectFormData{
+export function validateProjectBudgetAndTime(
+  projectData: ProjectFormData
+): ProjectFormData {
   // Inicializar valores predeterminados para evitar NaN si alguno es undefined
   const otherBudget = projectData.otherBudget || 0;
   const mechanicalBudget = projectData.mechanicalBudget || 0;
   const machineBudget = projectData.machineBudget || 0;
   const electricalBudget = projectData.electricalBudget || 0;
-  
+
   // Si no hay máquinas, calculamos el presupuesto total directamente de los campos del proyecto
-  if(projectData.machines.length === 0) {
+  if (projectData.machines.length === 0) {
     const updatedData: ProjectFormData = {
-      ...projectData, 
+      ...projectData,
       otherBudget,
       mechanicalBudget,
       machineBudget,
       electricalBudget,
-      totalBudget: otherBudget + mechanicalBudget + machineBudget + electricalBudget,
+      totalBudget:
+        otherBudget + mechanicalBudget + machineBudget + electricalBudget,
     };
 
     return updatedData;
   }
 
   // Si hay máquinas, calculamos los presupuestos y tiempos sumando los valores de todas las máquinas
-  const calculatedMachineBudget = projectData.machines.reduce((acc, machine) => acc + (machine.machineBudget || 0), 0);
-  const calculatedElectricalBudget = projectData.machines.reduce((acc, machine) => acc + (machine.electricalBudget || 0), 0);
-  const calculatedMechanicalBudget = projectData.machines.reduce((acc, machine) => acc + (machine.mechanicalBudget || 0), 0);
-  const calculatedOtherBudget = projectData.machines.reduce((acc, machine) => acc + (machine.otherBudget || 0), 0);
+  const calculatedMachineBudget = projectData.machines.reduce(
+    (acc, machine) => acc + (machine.machineBudget || 0),
+    0
+  );
+  const calculatedElectricalBudget = projectData.machines.reduce(
+    (acc, machine) => acc + (machine.electricalBudget || 0),
+    0
+  );
+  const calculatedMechanicalBudget = projectData.machines.reduce(
+    (acc, machine) => acc + (machine.mechanicalBudget || 0),
+    0
+  );
+  const calculatedOtherBudget = projectData.machines.reduce(
+    (acc, machine) => acc + (machine.otherBudget || 0),
+    0
+  );
 
   // Calculamos el presupuesto total sumando todos los presupuestos calculados
-  const calculatedTotalBudget = calculatedMachineBudget + calculatedElectricalBudget + calculatedMechanicalBudget + calculatedOtherBudget;
+  const calculatedTotalBudget =
+    calculatedMachineBudget +
+    calculatedElectricalBudget +
+    calculatedMechanicalBudget +
+    calculatedOtherBudget;
 
   const updatedData: ProjectFormData = {
     ...projectData,
@@ -241,12 +278,62 @@ export function validateProjectBudgetAndTime(projectData: ProjectFormData): Proj
     mechanicalBudget: calculatedMechanicalBudget,
     otherBudget: calculatedOtherBudget,
     totalBudget: calculatedTotalBudget,
-    otherTime: projectData.machines.reduce((acc, machine) => acc + (machine.otherTime || 0), 0),
-    electricalDesignTime: projectData.machines.reduce((acc, machine) => acc + (machine.electricalDesignTime || 0), 0),
-    mechanicalDesignTime: projectData.machines.reduce((acc, machine) => acc + (machine.mechanicalDesignTime || 0), 0),
-    assemblyTime: projectData.machines.reduce((acc, machine) => acc + (machine.assemblyTime || 0), 0),
-    developmentTime: projectData.machines.reduce((acc, machine) => acc + (machine.developmentTime || 0), 0)
+    otherTime: projectData.machines.reduce(
+      (acc, machine) => acc + (machine.otherTime || 0),
+      0
+    ),
+    electricalDesignTime: projectData.machines.reduce(
+      (acc, machine) => acc + (machine.electricalDesignTime || 0),
+      0
+    ),
+    mechanicalDesignTime: projectData.machines.reduce(
+      (acc, machine) => acc + (machine.mechanicalDesignTime || 0),
+      0
+    ),
+    assemblyTime: projectData.machines.reduce(
+      (acc, machine) => acc + (machine.assemblyTime || 0),
+      0
+    ),
+    developmentTime: projectData.machines.reduce(
+      (acc, machine) => acc + (machine.developmentTime || 0),
+      0
+    ),
   };
 
   return updatedData;
 }
+
+export function calculatePercentageBetweenDates(dates: Date[]): number {
+  if (dates.length < 2) {
+    return 0; // No se puede calcular el porcentaje con menos de dos fechas
+  }
+
+  const startDate = dates[0].getTime();
+  const endDate = dates[1].getTime();
+  const currentDate = new Date().getTime();
+
+  if (currentDate < startDate) {
+    return 0; // La fecha actual no está dentro del rango
+  }
+
+  if (currentDate > endDate) {
+    return 100; // La fecha actual está después del rango
+  }
+
+  const totalDuration = endDate - startDate;
+  const elapsedDuration = currentDate - startDate;
+
+  return Math.round((elapsedDuration / totalDuration) * 100);
+}
+
+export const formatTimeLineItemValue = (value: TableValue): Date[] | null => {
+  if (!value || !value.value) return null;
+
+  try {
+    const dates = JSON.parse(value.value) as Date[];
+    return dates.map((date) => new Date(date));
+  } catch (error) {
+    console.error("Error parsing item value:", error);
+    return null;
+  }
+};
