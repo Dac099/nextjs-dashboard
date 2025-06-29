d337cbc9bb84d9209769919a7e705f0f5ec907ed
David Ceja Zapata <davidcejazapata@gmail.com>
2025-06-29 13:07:28 -0600

    Feat: refactor setPercentageValue to return item and itemGroup, update percentage handling in UI

--- components/common/properties/percentage/actions.ts
+++ components/common/properties/percentage/actions.ts
@@ -4,172 +4,49 @@ import connection from '@/services/database';
 import { ItemData, ColumnData, ItemValue } from '@/utils/types/views';
 import { CustomError } from '@/utils/customError';
 
-export async function setPercentageValue(item: ItemData, column: ColumnData, value: Partial<ItemValue>): Promise<ItemValue>{
+export async function setPercentageValue(item: ItemData, column: ColumnData, value: Partial<ItemValue>): Promise<[ItemValue, ItemData]>{
   try {
     await connection.connect();
     const transacction = new sql.Transaction(connection);
     await transacction.begin();
-    console.log(2, 'Inicia transaccion');
+    
     try {
       const parsedValue = parseFloat(value.value!);
-      const requestGroupPosition = await transacction
-        .request()
-        .input('groupId', item.groupId)
-        .query('SELECT position FROM Groups WHERE id = @groupId');
-
-      const requestLastPositionGroups = await transacction
-        .request()
-        .input('groupId', item.groupId)
-        .query(`
-          SELECT MAX(position) AS lastPosition
-          FROM Groups
-          WHERE board_id = (
-            SELECT board_id 
-            FROM Groups
-            WHERE id = @groupId
-          )
-            AND deleted_at IS NULL
-        `);
-
-      const lastPosition = requestLastPositionGroups.recordset[0].lastPosition;
-      const groupPosition = requestGroupPosition.recordset[0].position;
+      const lastPosition = await getLastGroupId(transacction, item.groupId);
+      const groupPosition = await getCurrentGroupPosition(transacction, item.groupId);
       const isUpdateValue = value.id !== undefined;
-      console.log(1, lastPosition, groupPosition, isUpdateValue);
-      
-      if(parsedValue < 100 && groupPosition === lastPosition){
+
+      /** When the value is less than 100 just update or create tehe record */
+      if(parsedValue < 100){
         if (isUpdateValue) {
-          await transacction
-            .request()
-            .input('id', value.id)
-            .input('value', parsedValue)
-            .query(`
-              UPDATE TableValues 
-              SET value = @value,
-                updated_at = GETDATE()
-              WHERE id = @id`
-            );
-
-            await transacction.commit();
-            return {
-              id: value.id!,
-              itemId: item.id,
-              columnId: column.id,
-              value: parsedValue.toString(),
-            };
+          const itemUpdated = await updatePercentageRecord(item, column, value as ItemValue, transacction);
+          await transacction.commit();
+          return [itemUpdated, item];
+            
         } else {
-          const resultInsertion = await transacction
-            .request()
-            .input('itemId', item.id)
-            .input('columnId', column.id)
-            .input('value', parsedValue)
-            .query(`
-              INSERT INTO TableValues (item_id, column_id, value) 
-              OUTPUT INSERTED.id 
-              VALUES (@itemId, @columnId, @value)
-            `);
-
-            await transacction.commit();
-            return {
-              id: resultInsertion.recordset[0].id,
-              itemId: item.id,
-              columnId: column.id,
-              value: parsedValue.toString(),
-            };
+          const itemCreated = await createPercentageRecord(item, column, value.value!, transacction);
+          await transacction.commit();
+          return [itemCreated, item];
         }
       }
-
-      if(parsedValue === 100 && groupPosition < lastPosition){
-        if(isUpdateValue){
-          await transacction
-            .request()
-            .input('id', value.id)
-            .query(`
-              UPDATE TableValues
-              SET value = 0,  
-                updated_at = GETDATE(),
-              WHERE id = @id 
-            `);
-          const groupsListResult = await transacction
-            .request()
-            .query(`
-              SELECT id, position
-              FROM Groups
-              WHERE board_id = (
-                SELECT board_id 
-                FROM Groups
-                WHERE id = @groupId
-              )
-                AND deleted_at IS NULL  
-              ORDER BY position ASC
-            `);
-          const currentGroupIndex = groupsListResult.recordset.findIndex(group => group.id === item.groupId);
-          const nextGroup = groupsListResult.recordset[currentGroupIndex + 1];
-
-          await transacction
-            .request()
-            .input('nextGroupId', nextGroup.id)
-            .input('itemId', item.id)
-            .query(`
-              UPDATE Items
-              SET group_id = @nextGroupId,
-                updated_at = GETDATE()
-              WHERE id = @itemId
-            `);
-          await transacction.commit();
-          return {
-            id: value.id!,
-            itemId: item.id,
-            columnId: column.id,
-            value: '0',
-          };
-        }else {
-          const resultInsertion = await transacction
-            .request()
-            .input('itemId', item.id)
-            .input('columnId', column.id)
-            .input('value', parsedValue)
-            .query(`
-              INSERT INTO TableValues (item_id, column_id, value) 
-              OUTPUT INSERTED.id 
-              VALUES (@itemId, @columnId, @value)
-            `);
-          
-          const groupsListResult = await transacction
-            .request()
-            .query(`
-              SELECT id, position
-              FROM Groups
-              WHERE board_id = (
-                SELECT board_id 
-                FROM Groups
-                WHERE id = @groupId
-              )
-                AND deleted_at IS NULL  
-              ORDER BY position ASC
-            `);
-          const currentGroupIndex = groupsListResult.recordset.findIndex(group => group.id === item.groupId);
-          const nextGroup = groupsListResult.recordset[currentGroupIndex + 1];
-
-          await transacction
-            .request()
-            .input('nextGroupId', nextGroup.id)
-            .input('itemId', item.id)
-            .query(`
-              UPDATE Items
-              SET group_id = @nextGroupId,
-                updated_at = GETDATE()
-              WHERE id = @itemId
-            `);
-          await transacction.commit();
-          return {
-            id: resultInsertion.recordset[0].id,
-            itemId: item.id,
-            columnId: column.id,
-            value: parsedValue.toString(),
-          };
-        }
+      
+      /** The value is equal to 100  */
+      let itemToReturn : ItemValue;
+      if(isUpdateValue){
+        itemToReturn = await updatePercentageRecord(item, column, {...value as ItemValue, value: '0'}, transacction);
+      }else{
+        itemToReturn = await createPercentageRecord(item, column, '0', transacction);
       }
 
+      if(groupPosition === lastPosition){
+        return [itemToReturn, item];
+      }
+
+      const nextGroupId = await getNextGroupId(transacction, item.groupId);
+      item.groupId = nextGroupId;
+      await moveItemToNextGroup(transacction, item.id, nextGroupId);
+
+      return [itemToReturn, item];
     } catch (error) {
       transacction.rollback();
       console.error('Error setting percentage value:', error);
@@ -179,4 +56,105 @@ export async function setPercentageValue(item: ItemData, column: ColumnData, val
     console.error('Database connection error:', error);
     throw new CustomError(500, 'Error de conexión a la base de datos', 'Intente nuevamente o llave a sistemas');
   }
+}
+
+async function updatePercentageRecord(item: ItemData, column: ColumnData, value: ItemValue, transacction: sql.Transaction): Promise<ItemValue> {
+  const itemValue = value.value!;
+
+  await transacction
+    .request()
+    .input('id', value.id)
+    .input('value', itemValue)
+    .query(`
+      UPDATE TableValues 
+      SET value = @value,
+        updated_at = GETDATE()
+      WHERE id = @id`
+    );
+ 
+  return {
+    id: value.id!,
+    itemId: item.id,
+    columnId: column.id,
+    value: itemValue,
+  };
+}
+
+async function createPercentageRecord(item: ItemData, column: ColumnData, value: string, transacction: sql.Transaction): Promise<ItemValue> {
+  const resultInsertion = await transacction
+    .request()
+    .input('itemId', item.id)
+    .input('columnId', column.id)
+    .input('value', value)
+    .query(`
+      INSERT INTO TableValues (item_id, column_id, value) 
+      OUTPUT INSERTED.id 
+      VALUES (@itemId, @columnId, @value)
+    `);
+
+  return {
+    id: resultInsertion.recordset[0].id,
+    itemId: item.id,
+    columnId: column.id,
+    value: value,
+  };
+}
+
+async function getLastGroupId (transacction: sql.Transaction, groupId: string): Promise<number> {
+  const requestLastPositionGroups = await transacction
+    .request()
+    .input('groupId', groupId)
+    .query(`
+      SELECT MAX(position) AS lastPosition
+      FROM Groups
+      WHERE board_id = (
+        SELECT board_id 
+        FROM Groups
+        WHERE id = @groupId
+      )
+        AND deleted_at IS NULL
+    `);
+
+  const position = requestLastPositionGroups.recordset[0].lastPosition;
+  return typeof position === 'number' ? position : parseInt(position, 10);
+}
+
+async function getCurrentGroupPosition(transacction: sql.Transaction, groupId: string): Promise<number> {
+  const requestGroupPosition = await transacction
+    .request()
+    .input('groupId', groupId)
+    .query('SELECT position FROM Groups WHERE id = @groupId');
+  const position = requestGroupPosition.recordset[0].position;
+  return typeof position === 'number' ? position : parseInt(position, 10);
+}
+
+async function getNextGroupId(transacction: sql.Transaction, groupId: string): Promise<string> {
+  const groupsListResult = await transacction
+    .request()
+    .query(`
+      SELECT id, position
+      FROM Groups
+      WHERE board_id = (
+        SELECT board_id 
+        FROM Groups
+        WHERE id = @groupId
+      )
+        AND deleted_at IS NULL  
+      ORDER BY position ASC
+    `);
+  const currentGroupIndex = groupsListResult.recordset.findIndex(group => group.id === groupId);
+  return groupsListResult.recordset[currentGroupIndex + 1].id;
+}
+
+async function moveItemToNextGroup(transaction: sql.Transaction, itemId: string, nextGroupId: string): Promise<void> {
+  await transaction
+    .request()
+    .input('itemId', itemId)
+    .input('nextGroupId', nextGroupId)
+    .query(`
+      UPDATE Items 
+      SET group_id = @nextGroupId, 
+          updated_at = GETDATE() 
+      WHERE id = @itemId
+    `);
 }
--- components/common/properties/percentage/percentage.tsx
+++ components/common/properties/percentage/percentage.tsx
@@ -2,7 +2,7 @@
 import styles from './percentage.module.css';
 import { ItemData, ItemValue, ColumnData } from '@/utils/types/views';
 import { useBoardDataStore } from '@/stores/boardDataStore';
-import { useState, FocusEvent, KeyboardEvent, use } from 'react';
+import { useState, FocusEvent, KeyboardEvent } from 'react';
 import { setPercentageValue } from './actions';
 
 type Props = {
@@ -18,38 +18,38 @@ export function Percentage({ item, column, value }: Props) {
   const handleSubmitValue = async(e: FocusEvent | KeyboardEvent) => {
     if(e.type === 'blur' || (e.type === 'keydown' && (e as KeyboardEvent).key === 'Enter')) {
       try {
-        const itemValue: ItemValue = await setPercentageValue(item, column, {
+        const [itemValue, itemGroup]: ItemValue = await setPercentageValue(item, column, {
           ...value,
           value: inputValue,
         });
         
         const parsedValue = parseFloat(inputValue);
         const newGroups = [...groups];
-        //if value is less than 100, update the value and the store
-        //if the value is 100, find the id of the next group
-        //delete the item from de current group and add it to the next group
-        //update the store with the new item position
-        if(parsedValue < 100){
-          const groupIndex = newGroups.findIndex(group => group.id === item.groupId);
-          const itemIndex = newGroups[groupIndex].items.findIndex(i => i.id === item.id);
-          
-          if(value?.id === undefined){
-            newGroups[groupIndex].items[itemIndex].values.push(itemValue)
-          }else{
-            newGroups[groupIndex].items[itemIndex].values = item.values.map(val => {
-              if(val.id === value.id) {
-                return {
-                  ...val,
-                  value: inputValue,
-                };
-              }
-              return val;
-            });
+        const groupIndex = newGroups.findIndex(group => group.id === item.groupId);
+        const itemIndex = newGroups[groupIndex].items.findIndex(i => i.id === item.id);
+        const lastGroupIndex = newGroups.length - 1;
 
-          }
+        if(value?.id === undefined){
+          newGroups[groupIndex].items[itemIndex].values.push(itemValue)
+        }else{
+          newGroups[groupIndex].items[itemIndex].values = item.values.map(val => {
+            if(val.id === value.id) {
+              return {
+                ...val,
+                value: inputValue,
+              };
+            }
+            return val;
+          });
         }
 
-        setGroups(newGroups);
+        if(parsedValue < 100){          
+          setGroups(newGroups);
+          return;
+        }
+
+
+
       } catch (error) {
         console.log('Error al actualizar el valor de porcentaje:', error);
       }
