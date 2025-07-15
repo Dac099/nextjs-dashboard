02c6517fb8f7d527aa4602630d79eeb305dbc7dc
Dac099 <d.ceja@ynemx.com>
2025-07-15 10:50:38 -0600

    Refactor: update database connection logic to use switch statement for environment handling

--- services/database.ts
+++ services/database.ts
@@ -2,9 +2,21 @@ import sql from 'mssql';
 import { config } from 'dotenv';
 
 config();
-const isDevelopment: boolean = process.env.ENVIRONMENT === 'DEVELOPMENT';
-const sqlStringConnection: string = (isDevelopment ? process.env.CONNECTION_STRING_DEV : process.env.CONNECTION_STRING_PROD) as string;
+let sqlStringConnection: string | undefined;
 
+switch (process.env.ENVIRONMENT) {
+  case 'DEVELOPMENT':
+    sqlStringConnection = process.env.CONNECTION_STRING_DEV as string;
+    break;
+  case 'PRODUCTION':
+    sqlStringConnection = process.env.CONNECTION_STRING_PROD as string;
+    break;
+  case 'LOCAL':
+    sqlStringConnection = process.env.CONNECTION_STRING_LOCAL as string;
+    break;
+  default:
+    throw new Error('Invalid environment specified in .env file');
+}
 const connection: sql.ConnectionPool = new sql.ConnectionPool(sqlStringConnection);
 
 export default connection; 
